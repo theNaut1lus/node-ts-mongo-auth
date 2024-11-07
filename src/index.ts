15cbfa701161762e9a4b811686294641071b6444
Sidney Aulakh <sidneyaulakh@gmail.com>
2024-11-07 12:43:33 +1100

    first commit

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import http from 'http';
+import bodyparser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+import mongoose from 'mongoose';
+
+const app = express();
+
+app.use(cors({
+    credentials: true,
+}));
+
+app.use(bodyparser.json());
+app.use(cookieParser());
+app.use(compression());
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+    console.log('Server is running on port 8080');
+});
+
+const MONGO_URL = "mongodb+srv://sidneyaulakh:sidneyaulakh@cluster0.z1pzo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL)
+mongoose.connection.on('error', (error) => console.log(error));
+
+console.log("Hello, Sid!");
